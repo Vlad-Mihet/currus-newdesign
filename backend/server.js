3362200d95fd2e3453d73ac5589359dbe6db4eb1
Ian Jang <ianjang@u.northwestern.edu>
2021-01-31 22:26:22 +0900

    heroku

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/in
 //     res.send('Server is ready')
 // })
 
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`)
