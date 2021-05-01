7a639303a4ceed2338fa1e771db20f0b2bcc171a
Vlad Mihet <vlad.mihet@yahoo.com>
2021-05-01 15:16:14 +0300

    .

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/api/config/paypal", (req, res) => {
 //     res.send('Server is ready')
 // })
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
